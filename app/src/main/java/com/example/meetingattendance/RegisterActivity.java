4afe9904ce47233ac0be89306b973c57d61bfa40
Amanda Shafack <amandashafack@MacBook-Pro-de-Amanda.local>
2019-02-13 10:24:24 +0100

    User Interface of the Meeting Attendance App

---
+++ app/src/main/java/com/example/meetingattendance/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.meetingattendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+    }
+}
---
+++ app/src/main/java/com/example/meetingattendance/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.example.meetingattendance;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(getApplicationContext(),
+                RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
