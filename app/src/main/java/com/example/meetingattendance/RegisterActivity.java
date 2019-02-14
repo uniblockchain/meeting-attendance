73a32b89252a40befc4ee02eddbfe3fe8dbcd6aa
Amanda Shafack <amandashafack@MacBook-Pro-de-Amanda.local>
2019-02-14 09:15:02 +0100

    Reverted to inactive Register & Signup screens

--- app/src/main/java/com/example/meetingattendance/RegisterActivity.java
+++ app/src/main/java/com/example/meetingattendance/RegisterActivity.java
@@ -13,7 +13,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         // Set the dimensions of the sign-in button.
         SignInButton signInButton = findViewById(R.id.sign_in_button);
